aa46f6beb9f433f94214d28aac47605fb6db009e
Todsaporn (katopz) Banjerdkit <katopz@gmail.com>
2017-12-05 23:05:35 +0700

    feat(docker): Add multi stage docker build

--- index.js
+++
@@ -1,9 +0,0 @@
-// Pre
-require('./lib/pre');
-// Init server
-var app = new (require('./lib/WTServer'))().app;
-app.get('/', function (req, res) { return res.send('Hi!'); });
-// Start app
-require('./catcat')(app);
-// Start server
-module.exports = require('webtask-tools').fromExpress(app);
